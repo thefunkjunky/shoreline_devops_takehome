8529c7e8ac26992e700a817c0361ef1a2357f631
Garrett Anderson <g@devnull.rip>
2021-09-07 16:17:32 -0700

    AWS Networking Firewall update actions added

--- apply_firewalls.py
+++ apply_firewalls.py
@@ -9,10 +9,10 @@ from gcloud_firewall import get_creds_project, init_gcloud_client, apply_gcloud_
 from aws_firewall import init_aws_client, apply_aws_rule_group, apply_aws_firewall_resources
 
 
-def apply_gcloud_rule(client, firewall_rule, gcloud_configs):
+def apply_gcloud_rule(client, firewall_rule, project, gcloud_configs):
   """Parses and applies firewall rules for Google Cloud"""
   for network, configs in gcloud_configs["networks"].items():
-    apply_gcloud_firewall(client, firewall_rule, network, configs)
+    apply_gcloud_firewall(client, firewall_rule, project, network, configs)
 
 
 def apply_aws_rule(client, firewall_rule, aws_configs):
@@ -35,27 +35,28 @@ def main():
   with open(args.config, "r") as f:
     firewall_configs = yaml.safe_load(f)
 
-  if "aws" in firewall_config["providers"]:
-    aws_client = init_aws_client()
+  aws_client = None
+  gcloud_client = None
 
-  if "gcloud" in firewall_config["providers"]:
-    gcloud_credentials, gcloud_project = get_creds_project()
-    gcloud_client = init_gcloud_client(gcloud_credentials)
-
-  for firewall_name, firewall_config in firewall_configs["firewall_rules"].items():
+  for firewall_name, configs in firewall_configs["firewall_rules"].items():
     firewall_rule = FirewallRule(
-      cidrs=firewall_config["cidrs"],
-      protocol_ports=firewall_config["protocol_ports"],
-      action=firewall_config["action"]
+      cidrs=configs["cidrs"],
+      protocol_ports=configs["protocol_ports"],
+      action=configs["action"]
     )
-    if "gcloud" in firewall_config["providers"]:
-      apply_gcloud_rule(gcloud_client, firewall_rule, gcloud_project, firewall_config["providers"]["gcloud"])
+    if "gcloud" in configs["providers"]:
+      if not gcloud_client:
+        gcloud_credentials, gcloud_project = get_creds_project()
+        gcloud_client = init_gcloud_client(gcloud_credentials)
+      apply_gcloud_rule(gcloud_client, firewall_rule, gcloud_project, configs["providers"]["gcloud"])
 
-    if "aws" in firewall_config["providers"]:
-      apply_aws_rule(aws_client, firewall_rule, firewall_config["providers"]["aws"])
+    if "aws" in configs["providers"]:
+      if not aws_client:
+        aws_client = init_aws_client()
+      apply_aws_rule(aws_client, firewall_rule, configs["providers"]["aws"])
 
-  if "aws" in firewall_config["additional_provider_configs"]:
-    apply_aws_firewall_resources(aws_client, firewall_config["additional_provider_configs"]["aws"])
+  if "aws" in firewall_configs["additional_provider_configs"]:
+    apply_aws_firewall_resources(aws_client, firewall_configs["additional_provider_configs"]["aws"])
 
 
 if __name__ == '__main__':
--- aws_firewall.py
+++ aws_firewall.py
@@ -3,6 +3,7 @@ Applies firewall rules to AWS resources
 """
 
 import boto3
+import botocore
 
 protocol_map = {
   "tcp": 6,
@@ -21,30 +22,74 @@ def init_aws_client():
   return client
 
 
-def create_firewall(client, firewall_name, firewall_configs):
-  """Creates an AWS Network Firewall"""
-  subnets = [{"SubnetId": subnet} for subnet in firewall_configs["subnet_ids"]]
-  policy_response = client.describe_firewall_policy(
-    FirewallPolicyName=firewall_configs["policy"]
+def get_firewall(client, firewall_name):
+  """Returns GET response for AWS Networking Firewall"""
+  response = client.describe_firewall(
+    FirewallName=firewall_name,
   )
+  return response
+
+
+def get_policy(client, policy_name):
+  """Returns GET response for AWS Networking policy"""
+  response = client.describe_firewall_policy(
+    FirewallPolicyName=policy_name,
+  )
+  return response
+
+
+def get_rule_group(client, rule_group_name):
+  """Returns GET response for AWS Networking rule group"""
+  response = client.describe_rule_group(
+    RuleGroupName=rule_group_name,
+    Type="STATELESS"
+  )
+  return response
+
+
+def apply_firewall(client, firewall_name, firewall_configs):
+  """Creates/updates an AWS Network Firewall"""
+  subnets = [{"SubnetId": subnet} for subnet in firewall_configs["subnet_ids"]]
+  policy_response = get_policy(client, firewall_configs["policy"])
   policy_arn = policy_response["FirewallPolicyResponse"]["FirewallPolicyArn"]
+
+  # Check if firewall exists and updates the policy if necessary
+  try:
+    get_firewall_response = get_firewall(client, firewall_name)
+    print(f"AWS Firewall {firewall_name} already exists.")
+    current_policy_arn = get_firewall_response["Firewall"]["FirewallPolicyArn"]
+    if current_policy_arn != policy_arn:
+      print("Updating policy association...")
+      # This needs retry/backoff logic in case UpdateToken is in use
+      update_token = get_firewall_response["UpdateToken"]
+      update_response = client.associate_firewall_policy(
+        UpdateToken=update_token,
+        FirewallName=firewall_name,
+        FirewallPolicyArn=policy_arn
+      )
+      return update_response
+    else:
+      print(f"Policy on AWS Firewall {firewall_name} hasn't changed, skipping...")
+      return None
+  except client.exceptions.ResourceNotFoundException:
+    print(f"Creating AWS Firewall {firewall_name}...")
+
   response = client.create_firewall(
     FirewallName=firewall_name,
-    FirewallPolicyArn=firewall_configs["policy"],
+    FirewallPolicyArn=policy_arn,
     VpcId=firewall_configs["vpc_id"],
     SubnetMappings=subnets
   )
+  return response
 
 
-def create_firewall_policy(client, policy_name, policy_configs):
-  """Creates an AWS Network Firewall policy"""
+def apply_firewall_policy(client, policy_name, policy_configs):
+  """Creates/updates an AWS Network Firewall policy"""
   rule_groups = []
   rule_group_arns = []
   for group, rule_config in policy_configs["rule_groups"].items():
-    group_response = client.describe_rule_group(
-      RuleGroupName=group
-    )
-    arn = group_response["RuleGroupResponse"]["RuleGroupArn"]
+    get_group_response = get_rule_group(client, group)
+    arn = get_group_response["RuleGroupResponse"]["RuleGroupArn"]
     rule_groups.append(
       {
         "ResourceArn": arn,
@@ -52,7 +97,25 @@ def create_firewall_policy(client, policy_name, policy_configs):
       }
     )
 
-  print(f"Creating AWS Firewall policy {policy_name}")
+  # Check if policy exists and updates it
+  try:
+    get_response = get_policy(client, policy_name)
+    print(f"AWS Firewall policy {policy_name} already exists. Updating...")
+    # This needs retry/backoff logic in case UpdateToken is in use
+    update_token = get_response["UpdateToken"]
+    response = client.update_firewall_policy(
+      UpdateToken=update_token,
+      FirewallPolicyName=policy_name,
+      FirewallPolicy={
+        "StatelessRuleGroupReferences": rule_groups,
+        "StatelessDefaultActions": ["aws:drop"],
+        "StatelessFragmentDefaultActions": ["aws:drop"]
+      }
+    )
+    return response
+  except client.exceptions.ResourceNotFoundException:
+    print(f"Creating AWS Firewall policy {policy_name}...")
+    
   response = client.create_firewall_policy(
     FirewallPolicyName=policy_name,
     FirewallPolicy={
@@ -64,8 +127,8 @@ def create_firewall_policy(client, policy_name, policy_configs):
   return response
 
 
-def create_rule_group(client, firewall_rule, aws_configs):
-  """Creates an AWS Network Firewall rule group"""
+def apply_rule_group(client, firewall_rule, aws_configs):
+  """Creates/updates an AWS Network Firewall rule group"""
 
   stateless_rules = []
 
@@ -80,6 +143,7 @@ def create_rule_group(client, firewall_rule, aws_configs):
   sources_capacity = len(sources) if len(sources) > 0 else 1
   protocols_capacity = len(sources) if len(firewall_rule.protocol_ports) > 0 else 1
 
+  # I don't understand this, but it seems to work
   capacity *= sources_capacity * protocols_capacity
 
   for protocol, ports in firewall_rule.protocol_ports.items():
@@ -112,11 +176,31 @@ def create_rule_group(client, firewall_rule, aws_configs):
   if "add_to_capacity" in aws_configs:
     capacity += aws_configs["add_to_capacity"]
 
-  print(f"Creating AWS Firewall rule group {aws_configs['rule_group']}...")
+  # Check if rule group exists and updates it
+  try:
+    get_response = get_rule_group(client, name)
+    print(f"AWS Firewall rule group {name} exists. Updating...")
+    update_token = get_response["UpdateToken"]
+    response = client.update_rule_group(
+      UpdateToken=update_token,
+      RuleGroupName=name,
+      Type="STATELESS",
+      RuleGroup={
+        "RulesSource": {
+          "StatelessRulesAndCustomActions": {
+            "StatelessRules": stateless_rules
+          }
+        }
+      }
+    )
+    return response
+  except client.exceptions.ResourceNotFoundException:
+    print(f"Creating AWS Firewall rule group {name}...")
+
   response = client.create_rule_group(
     Capacity=capacity,
     Type="STATELESS",
-    RuleGroupName=aws_configs["rule_group"],
+    RuleGroupName=name,
     RuleGroup={
       "RulesSource": {
         "StatelessRulesAndCustomActions": {
@@ -128,21 +212,14 @@ def create_rule_group(client, firewall_rule, aws_configs):
   return response
 
 
-
 def apply_aws_rule_group(client, firewall_rule, aws_configs):
   """Creates/updates AWS Network Firewall rule groups"""
-  try:
-    rule_group_response = client.describe_rule_group(
-      RuleGroupName=aws_configs["rule_group"]
-    )
-  except:
-    rule_group_response = create_rule_group(client, firewall_rule, aws_configs)
-  rule_group_arn = rule_group_response["RuleGroupResponse"]["RuleGroupArn"]
+  rule_group_response = apply_rule_group(client, firewall_rule, aws_configs)
 
 
 def apply_aws_firewall_resources(client, firewall_configs):
   """Creates/updates AWS Firewall policies and firewalls"""
-  for policy, policy_config in firewall_configs["policies"].items():  
-    policy_response = create_firewall_policy(client, policy, policy_config)
+  for policy, policy_config in firewall_configs["policies"].items():
+    policy_response = apply_firewall_policy(client, policy, policy_config)
   for firewall, firewall_config in firewall_configs["firewalls"].items():
-    firewall_response = create_firewall(client, firewall, firewall_config)
+    firewall_response = apply_firewall(client, firewall, firewall_config)
